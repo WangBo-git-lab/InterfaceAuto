f4118c169fed0e6b6f72a400009aa58fff17166a
Wang Bo <wangbo@jiazhengye.cn>
2024-12-14 15:22:38 +0800

    多接口框架

--- common.py
+++ common.py
@@ -1,9 +1,34 @@
 import requests
-
+from websocket import create_connection
 
 class Common(object):
-    def __init__(self, url_root):
-        self.url_root = url_root
+    def __init__(self, url_root, api_type):
+        '''
+        :param api_type:接口类似当前支持http，ws，http就是http协议，ws是Websocket协议
+        :param url_root: 被测系统的跟路由
+        '''
+        if api_type == 'ws':
+            self.ws = create_connection(url_root)
+        elif api_type == 'http':
+            self.ws = 'null'
+            self.url_root = url_root
+
+    def send(self, params):
+        '''
+        :param params: websocket接口的参数
+        :return: 访问接口的返回值
+        '''
+
+        self.ws.send(params)
+        res = self.ws.recv()
+        return res
+
+    def __del__(self):
+        '''
+        :return:
+        '''
+        if self.ws != 'null':
+            self.ws.close()
 
     def get(self, uri, params=''):
         if params:
@@ -26,4 +51,20 @@ class Common(object):
             res = requests.post(url, data=params, headers=headers)
         else:
             res = requests.post(url, headers=headers)
+        return res
+
+    def put(self,uri,params=None):
+        url = self.url_root + uri
+        if params is not None:
+            res = requests.put(url,data=params)
+        else:
+            res = requests.put(url)
+        return res
+
+    def delete(self,uri,params=None):
+        url = self.url_root + uri
+        if params is not None:
+            res = requests.delete(url, data=params)
+        else:
+            res = requests.delete(url)
         return res
--- login.py
+++ login.py
@@ -6,7 +6,7 @@ mobile = '15137139921'
 password = 'xyz1230.'
 xmjz_time = int(time.time())
 payload = f'mobile={mobile}&password={password}&xmjz_time={xmjz_time}'
-comm = Common('https://appapi-test.jiazhengye.cn')
+comm = Common('https://appapi-test.jiazhengye.cn','http')
 response_login = comm.post(uri, params=payload)
 if response_login.status_code == 200:
     print('Response内容：' + response_login.text)
